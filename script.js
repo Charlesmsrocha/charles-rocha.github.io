8735b62cd256bc67ed01ca1b25ca7e34279c7d2e
Charles Rocha <59806026+Charlesmsrocha@users.noreply.github.com>
2025-08-21 23:30:45 +0100

    The idea needs some adjustments
    I need to understand how to make the quiz in a easier way.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // script.js
-    const questionEl = document.getElementById('question');
+    // const questionEl = document.getElementById('question1');
     const optionsEl = document.querySelectorAll('.option');
     const resultEl = document.getElementById('result');
 
@@ -16,4 +16,20 @@
             }
         });
     });
-    
+
+    const optionsEl2 = document.querySelectorAll('.option2');
+    const resultEl2 = document.getElementById('result2');
+
+    const correctAnswer2 = 'POP3.'; // The right answer
+
+    optionsEl2.forEach(option => {
+        option.addEventListener('click', () => {
+            if (option.textContent === correctAnswer2) {
+                resultEl2.textContent = "Correct!";
+                resultEl2.className = 'correct'; // Add class to green
+            } else {
+                resultEl2.textContent = "Incorrect!";
+                resultEl2.className = 'incorrect'; // Add class to red
+            }
+        });
+    });
