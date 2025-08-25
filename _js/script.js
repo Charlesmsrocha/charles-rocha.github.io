6bf16df4bc8c866e514c8fd1cd3faa079086c1b0
Charles Rocha <59806026+Charlesmsrocha@users.noreply.github.com>
2025-08-25 20:26:54 +0100

    Appearance adjustments
    More questions added and some standardization on the visual.

--- _js/script.js
+++ _js/script.js
@@ -1,45 +1,9 @@
-// script.js
-    // // const questionEl = document.getElementById('question1');
-    // const optionsEl = document.querySelectorAll('.option');
-    // const resultEl = document.getElementById('result');
-
-    // const correctAnswer = 'Communication between microservices is carried out through fixed and predefined routes, simplifying the network topology in static environments.'; // The right answer
-
-    // optionsEl.forEach(option => {
-    //     option.addEventListener('click', () => {
-    //         if (option.textContent === correctAnswer) {
-    //             resultEl.textContent = "Correct!";
-    //             resultEl.className = 'correct'; // Add class to green
-    //         } else {
-    //             resultEl.textContent = "Incorrect!";
-    //             resultEl.className = 'incorrect'; // Add class to red
-    //         }
-    //     });
-    // });
-
-    // const optionsEl2 = document.querySelectorAll('.option2');
-    // const resultEl2 = document.getElementById('result2');
-
-    // const correctAnswer2 = 'POP3.'; // The right answer
-
-    // optionsEl2.forEach(option => {
-    //     option.addEventListener('click', () => {
-    //         if (option.textContent === correctAnswer2) {
-    //             resultEl2.textContent = "Correct!";
-    //             resultEl2.className = 'correct'; // Add class to green
-    //         } else {
-    //             resultEl2.textContent = "Incorrect!";
-    //             resultEl2.className = 'incorrect'; // Add class to red
-    //         }
-    //     });
-    // });
-
 let questions = [];
 let currentQuestionIndex = 0;
 let score = 0;
 
 async function loadQuestions() {
-  const response = await fetch("questions.json");
+  const response = await fetch("../questions.json");
   questions = await response.json();
   showQuestion();
 }
